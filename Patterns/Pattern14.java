55e4e84ab51f3c68cd9803c9cbf16d0ef48c1134
jeeban107 <jeevanbehera107@gmail.com>
2024-06-01 10:44:23 +0530

    Day 3
    co

--- Patterns/Pattern14.java
+++ Patterns/Pattern14.java
@@ -7,9 +7,11 @@ public class Pattern14 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 0; i <n ; i++) {
-            for (int j = 0; j <i ; j++) {
-                
-            }
+           for ( char  ch ='A'; ch <='A' +i; ch++)
+           {
+               System.out.print(ch);
+           }
+            System.out.print("\n");
         }
     }
 }
---
+++ Patterns/Pattern15.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern15 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+        for (int i = 0; i <n ; i++) {
+
+        for (char ch = 'A'; ch <= 'A'+( n - i - 1 ); ch++) {
+            System.out.print(ch);
+        }
+        System.out.print('\n');
+        }
+    }
+
+}
