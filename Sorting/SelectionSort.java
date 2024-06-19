16c4f5330f5e2015626b0abd03ca9cb90dab6e15
jeeban107 <jeevanbehera107@gmail.com>
2024-06-19 15:14:04 +0530

    error
    corrected

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -7,9 +7,9 @@ public class SelectionSort {
 
     static  void  SelSort(int arr[] ,int n){
 
-        for (int i = 0; i <n ; i++) {
+        for (int i = 0; i <=n-2; i++) {
             int min = i;
-            for (int j = i; j <n ; j++) {
+            for (int j = i; j <=n-1 ; j++) {
                 if(arr[j]< arr[min]){
                     min = j;
                 }
